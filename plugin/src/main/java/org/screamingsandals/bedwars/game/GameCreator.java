56a303cdd952f4e362ad79e4e94d7f799863a831
Frawlah <88870560+Frawlah80@users.noreply.github.com>
2024-02-24 13:51:08 +0530

    dont let me make changes on mobile
    This should do the same thing as last commit, just left a couple {} that led to syntax err dw

--- plugin/src/main/java/org/screamingsandals/bedwars/game/GameCreator.java
+++ plugin/src/main/java/org/screamingsandals/bedwars/game/GameCreator.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2023 ScreamingSandals
- *
- * This file is part of Screaming BedWars.
- *
- * Screaming BedWars is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Screaming BedWars is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Screaming BedWars. If not, see <https://www.gnu.org/licenses/>.
- */
-
 package org.screamingsandals.bedwars.game;
 
 import org.bukkit.*;
@@ -257,9 +238,9 @@ public class GameCreator {
                     response = i18n("admin_command_set_spec_before_save");
                 } // else if (game.getGameStoreList().isEmpty()) {
                   //  response = i18n("admin_command_set_stores_before_save");
-                } // else if (game.getSpawners().isEmpty()) {
+                /*}*/ // else if (game.getSpawners().isEmpty()) {
                   //  response = i18n("admin_command_set_spawners_before_save");
-                } else {
+                /*}*/ else {
                     game.saveToConfig();
                     game.start();
                     Main.addGame(game);
